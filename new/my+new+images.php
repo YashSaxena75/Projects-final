edc552bccd3c4ea1c28885585e45c929010e53d3
Yash Saxena <35332582+YashSaxena75@users.noreply.github.com>
2020-03-25 04:38:24 +0530

    Add files via upload

---
+++ new/my+new+images.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>HacLabs</title>
+ <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300">
+      <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+<div id="menu-nav">
+  <div id="navigation-bar">
+    <ul>
+      <li><a href="form.php"><i class="fa fa-home"></i><span>Home</span></a></li>
+      <li><a href="#"><i class="fa fa-handshake"></i><span>Services</span></a></li>
+      <li><a href="#"><i class="fa fa-user"></i><span>About</span></a></li>
+      <li><a href="form1.php"><i class="fa fa-book"></i><span>Contact</span></a></li>
+    </ul>
+  </div>
+  </div>
+<br><br>
+<h2 class="ptag">My images folder</h2>
+<br>
+<a class="ptag" href="bob1.jpeg" download="bob1.jpeg">bob1.jpeg</a><br>
+<a class="ptag" href="bob2.jpeg" download="bob2.jpeg">bob2.jpeg</a><br>
+<a class="ptag" href="bob3.jpeg" download="bob3.jpeg">bob3.jpeg</a><br>
+<a class="ptag" href="bob4.png" download="bob4.png">bob4.png</a><br>
+
+</body>
+</html>
---
+++ new/my_order_.php
@@ -0,0 +1,56 @@
+<html>
+<meta charset="UTF-8">
+  <title>HacLabs</title>
+ <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300">
+      <link rel="stylesheet" href="style1.css">
+</head>
+<body>
+<div class="login">
+	<h1>Track Order</h1>
+    <form method="POST">
+        <input type="text" name="pass" placeholder="PIN" required="required" />
+        <button type="submit" class="btn btn-primary btn-block btn-large">Track</button>
+    </form>
+</div>
+</body>
+</html>
+
+<?php
+$passs=$_POST['pass'];
+$substitutions = array(
+';'=>'','; '=>'','&'=>'','& '=>'','&&'=>'','&& '=>'','| '=>'','|| '=>'',
+'nc'=>'', '/bin/bash'=>'', '-e'=>'', 'hex'=>'', 'mkdir'=>'' ,'bash'=>'',
+'sh'=>'', '/dev'=>'', '/tcp'=>'', 'perl'=>'',
+'-e'=>'', 'socket'=>'', 'STDOUT'=>'', 'python'=>'', 'import'=>'',
+'php'=>'', '-r'=>'', 'rm'=>'', 'mkfifo'=>'', 'base64'=>'', 'Runtime'=>'',
+'xterm'=>'' ,'nest'=>'', 'host'=>'');
+
+
+$PIN='244713';
+if($passs=='244713')
+{
+echo "<script>alert('order shipped')</script>";
+}
+else if ($passs!='244713')
+{
+
+if (strpos($passs,$PIN) !== false) {
+    $fg=1;
+}
+else
+{
+$fg=2;
+}
+if($fg==1)
+{
+$passs = str_replace( array_keys( $substitutions ), $substitutions, $passs );
+$ot=shell_exec(' ping -c 3 '.$passs);
+echo "<pre>{$ot}</pre";
+}
+if($fg==2)
+{
+echo "<script>alert('wrong PIN')</script>";
+}
+}
+?>
